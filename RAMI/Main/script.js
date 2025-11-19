f48dc5dd1a00afc57f425abb043961b834d8b72f
18215benben <amitbenben@gmail.com>
2025-11-19 16:39:35 +0000

    Add files via upload

---
+++ RAMI/Main/script.js
@@ -0,0 +1,13 @@
+document.getElementById("Menu_Button").onclick = function() {
+    var menu = document.getElementById("menu");
+    var content = document.getElementById("Menu_Button")
+    if (menu.style.visibility === "visible") {
+        menu.style.visibility = "hidden";
+        content.innerHTML = "Menu";
+        content.style.left = "10px";
+    } else {
+        menu.style.visibility = "visible";
+        content.innerHTML = "❌";
+        content.style.left = "12%";
+    }
+}
